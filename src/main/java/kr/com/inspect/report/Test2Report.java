8a4e52c5116d73ee054fd1f8d160a992659a6fdf
Yeonhee Kim <10yeony@gmail.com>
2020-11-01 23:51:35 +0900

    [2020-11-01 23:51] Record Changes

--- src/main/java/kr/com/inspect/report/Test2Report.java
+++ src/main/java/kr/com/inspect/report/Test2Report.java
@@ -9,17 +9,18 @@ import kr.com.inspect.dto.Sound;
 import kr.dogfoot.hwplib.object.HWPFile;
 import kr.dogfoot.hwplib.object.bodytext.Section;
 import kr.dogfoot.hwplib.object.bodytext.paragraph.Paragraph;
+import kr.dogfoot.hwplib.object.bodytext.paragraph.text.ParaText;
 import kr.dogfoot.hwplib.tool.blankfilemaker.BlankFileMaker;
 import kr.dogfoot.hwplib.writer.HWPWriter;
 
-public class TestReport {
+public class Test2Report {
 
 	public static void main(String[] args) {
 		List<Sound> list = new ArrayList<>();
 		list.add(new Sound("테스트1", "테스트1", "테스트1", "테스트1", "테스트1"));
 		list.add(new Sound("테스트2", "테스트2", "테스트2", "테스트2", "테스트2"));
 		String path = "C:\\kyh\\export\\excel\\";
-		TestReport testReport = new TestReport();
+		Test2Report testReport = new Test2Report();
 		testReport.run(path, list);
 
 	}
@@ -31,13 +32,10 @@ public class TestReport {
 		
 		try {
 			HWPFile hwpFile = BlankFileMaker.make();
-			Section s1 = hwpFile.getBodyText().getSectionList().get(0);
-			Paragraph firstParagraph = s1.getParagraph(0);
-			firstParagraph.getText().addString("이것은 추가된 문자열입니다.");
-			Section s2 = hwpFile.getBodyText().getSectionList().get(1);
-			Paragraph secondParagraph = s2.getParagraph(0);
-			secondParagraph.getText().addString("\n");
-			secondParagraph.getText().addString("이것은 추가된 문자열입니다.2");
+			Paragraph p = hwpFile.getBodyText().getSectionList().get(0).addNewParagraph();
+			p.createText();
+			ParaText pt = p.getText();
+			pt.addString("테스트~~~");
 			HWPWriter.toFile(hwpFile, path+hwpFileName );
 		}catch(Exception e) {
 			//e.printStackTrace();
