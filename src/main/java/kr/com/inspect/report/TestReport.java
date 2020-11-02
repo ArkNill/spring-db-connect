e3d94179693f32586d718c34b87c6c214afda711
Yeonhee Kim <10yeony@gmail.com>
2020-11-02 14:58:08 +0900

    [2020-11-02 14:58] Add navigator.jsp

---
+++ src/main/java/kr/com/inspect/report/TestReport.java
@@ -0,0 +1,95 @@
+package kr.com.inspect.report;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.springframework.beans.factory.annotation.Value;
+
+import kr.com.inspect.dto.Sound;
+
+public class TestReport {
+	@Value("${table.column0}") 
+	private String column0;
+	
+	@Value("${table.column1}") 
+	private String column1;
+	
+	@Value("${table.column2}") 
+	private String column2;
+	
+	@Value("${table.column3}") 
+	private String column3;
+	
+	@Value("${table.column4}") 
+	private String column4;
+	
+	public void writeDocx(String path, List<Sound> list) {
+		String docxFileName = 
+				new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date()) 
+				+ "_log.docx";
+		
+		XWPFDocument doc = new XWPFDocument();
+		XWPFTable table = doc.createTable(list.size()+1, 5);
+		
+		/* 헤더 정보 구성 */
+		table.getRow(0).getCell(0).setText(column0);
+		table.getRow(0).getCell(1).setText(column1);
+		table.getRow(0).getCell(2).setText(column2);
+		table.getRow(0).getCell(3).setText(column3);
+		table.getRow(0).getCell(4).setText(column4);
+		
+		Sound vo;
+		for(int rowIdx=0; rowIdx < list.size(); rowIdx++) {
+			vo = list.get(rowIdx);
+			table.getRow(rowIdx+1).getCell(0).setText(vo.getId());
+			table.getRow(rowIdx+1).getCell(1).setText(vo.getCategory());
+			table.getRow(rowIdx+1).getCell(2).setText(vo.getTitle());
+			table.getRow(rowIdx+1).getCell(3).setText(vo.getCompany());
+			table.getRow(rowIdx+1).getCell(4).setText(vo.getContent());
+		}
+		
+		// 입력된 내용 파일로 쓰기
+		File file = new File(path + docxFileName);
+		FileOutputStream fos = null;
+		System.out.println(path + docxFileName);
+				
+		try {
+			fos = new FileOutputStream(file);
+			doc.write(fos);
+		} catch (FileNotFoundException e) {
+			//e.printStackTrace();
+		} catch (IOException e) {
+			//e.printStackTrace();
+		} finally {
+			try {
+				if(doc!=null) doc.close();
+				if(fos!=null) fos.close();
+			} catch (IOException e) {
+				//e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		TestReport testReport = new TestReport();
+		List<Sound> list = new ArrayList<>();
+		Sound sound = new Sound();
+		sound.setId("테스트1");
+		sound.setCompany("테스트2");
+		sound.setCategory("테스트3");
+		sound.setContent("테스트4");
+		sound.setTitle("테스트5");
+		list.add(sound);
+		testReport.writeDocx("C:\\kyh\\", list);
+	}
+	
+	
+}
--- src/main/webapp/WEB-INF/views/navigator.jsp
+++ src/main/webapp/WEB-INF/views/navigator.jsp
@@ -8,6 +8,9 @@
 </head>
 <body>
 <h2>스프링 프레임워크를 통한 DB 연동</h2>
-로그인 기능을 구현하시고 navigator.jsp로 이동할 수 있게 해주세요.
+<a href="elasticPage">Elasticsearch</a><br/>
+<a href="mongoPage">MongoDB</a><br/>
+<a href="postgrePage">PostgreSQL</a><br/>
+<a href="reportPage">보고서 파일 생성</a><br/>
 </body>
 </html>
